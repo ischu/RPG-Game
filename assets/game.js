dc5ae0555ce7567605a1ba050ab82a07fc4461e9
ianschu <ianschu7@gmail.com>
2018-11-23 14:46:21 -0500

    functions to choose character and defender now only run once

--- assets/game.js
+++ assets/game.js
@@ -1,21 +1,30 @@
 
 var noDefender = true;
+var noCharChosen = true;
 // jQuery code goes in this
 $(document).ready(function () {
     // selects character
-    $("div .unchosen").on("click", function () {
+    $("div.unchosen").on("click", function () {
+        // only runs if player has not chosen a character to play as
+        if(noCharChosen){
         // moves clicked character box to "your character" section
         $("#yourChar").append(this);
         // removes "unchosen" class from chosen character
         $(this).removeClass("unchosen");
+        // moves unchosen characters to the "available enemies" section
+        $("#availEnemies").append($(".unchosen"));
         // adds "enemies" class to unchosen characters
         $(".unchosen").addClass("enemies");
-        // removes .unchosen from enemies, allowing next function to work
+        // removes .unchosen from enemies
         $(".enemies").removeClass("unchosen");
-        // moves enemy characters to the "available enemies" section
-        $("#availEnemies").append($(".enemies"));
+        console.log("character chosen")
+        noCharChosen=false;
+        }
+        else{
+            console.log("noCharChosen = "+ noCharChosen)
+        }
         // selects defender
-        $("div .enemies").on("click", function () {
+        $("div.enemies").on("click", function () {
             // only works if no defender has been chosen yet
             if (noDefender) {
                 // moves clicked character box to "defender" section
@@ -24,10 +33,12 @@ $(document).ready(function () {
                 $(this).removeClass("enemies");
                 // adds "defender" class to chosen character
                 $(this).addClass("defender");
+                console.log("defender chosen")
                 noDefender = false;
+                return noDefender;
             }
             else {
-                console.log("defender already chosen!");
+                console.log("noDefender = " + noDefender);
             }
         });
     });
