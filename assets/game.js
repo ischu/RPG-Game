487f1a34eeeb11c74deb2adfb23a583c25a9c2d6
ianschu <ianschu7@gmail.com>
2018-11-23 13:58:15 -0500

    added jQuery for selecting character

--- assets/game.js
+++ assets/game.js
@@ -1,5 +1,17 @@
-console.log("javascript");
 
-$(document).ready(function(){
-// jQuery code goes in here
+
+// jQuery code goes in this
+$(document).ready(function () {
+    // makes character boxes clickable
+    $(".charBox").click(function () {
+        // moves clicked character box to "your character" section
+        $("#yourChar").append(this);
+        // removes "unchosen" class from chosen character
+        $(this).removeClass("unchosen");
+        // adds "enemies" class to unchosen characters
+        $(".unchosen").addClass("enemies");
+        // moves unchosen characters to the "available enemies" section
+        $("#availEnemies").append($(".enemies"));
+    });
+
 });
