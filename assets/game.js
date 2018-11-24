54c74ef4a2d19075236c2f7cd38db1dd9ff5fde3
ianschu <ianschu7@gmail.com>
2018-11-23 20:31:12 -0500

    started work on attack button

--- assets/game.js
+++ assets/game.js
@@ -1,27 +1,60 @@
-
+// character stats
+var obi = {
+    name: "obi",
+    healthPoints: 200,
+    attackPower: 10,
+    counterPower: 10,
+};
+var luke = {
+    name: "luke",
+    healthPoints: 102,
+    attackPower: 11,
+    counterPower: 10,
+};
+var vader = {
+    name: "vader",
+    healthPoints: 104,
+    attackPower: 12,
+    counterPower: 10,
+};
+var maul = {
+    name: "maul",
+    healthPoints: 155,
+    attackPower: 13,
+    counterPower: 10,
+};
+// array containing stat objects
+var charArray = [obi, luke, vader, maul];
+// "flag" variables for choosing if function should run
 var noDefender = true;
 var noCharChosen = true;
 // jQuery code goes in this
 $(document).ready(function () {
+    $("#obi").append("HP:" + obi.healthPoints);
+    $("#luke").append("HP:" + luke.healthPoints);
+    $("#vader").append("HP:" + vader.healthPoints);
+    $("#maul").append("HP:" + maul.healthPoints);
     // selects character
     $("div.unchosen").on("click", function () {
         // only runs if player has not chosen a character to play as
-        if(noCharChosen){
-        // moves clicked character box to "your character" section
-        $("#yourChar").append(this);
-        // removes "unchosen" class from chosen character
-        $(this).removeClass("unchosen");
-        // moves unchosen characters to the "available enemies" section
-        $("#availEnemies").append($(".unchosen"));
-        // adds "enemies" class to unchosen characters
-        $(".unchosen").addClass("enemies");
-        // removes .unchosen from enemies
-        $(".enemies").removeClass("unchosen");
-        console.log("character chosen")
-        noCharChosen=false;
+        if (noCharChosen) {
+            // moves clicked character box to "your character" section
+            $("#yourChar").append(this);
+            // removes "unchosen" class from chosen character
+            $(this).removeClass("unchosen");
+            // moves unchosen characters to the "available enemies" section
+            $("#availEnemies").append($(".unchosen"));
+            // adds "enemies" class to unchosen characters
+            $(".unchosen").addClass("enemies");
+            // removes .unchosen from enemies
+            $(".enemies").removeClass("unchosen");
+            // sets charName to the id of chosen character (used later to select corresponding object)
+            charName = this.id.toString();
+            console.log(charName + " chosen");
+            noCharChosen = false;
         }
-        else{
-            console.log("noCharChosen = "+ noCharChosen)
+        else {
+            console.log("noCharChosen = " + noCharChosen)
         }
         // selects defender
         $("div.enemies").on("click", function () {
@@ -41,5 +74,14 @@ $(document).ready(function () {
                 console.log("noDefender = " + noDefender);
             }
         });
+    });
+    // attack button
+    $("#attackButton").on("click", function () {
+        console.log(charName)
+        for (i = 0; i < charArray.length; i++) {
+            if (charName === charArray[i].name) {
+                console.log(charArray[i].name + " has an attack power of " + charArray[i].attackPower);
+            }
+        }
     });
 });
